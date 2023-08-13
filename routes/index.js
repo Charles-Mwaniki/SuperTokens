011bc7dffd0e41be84ef720e2fe5368827201e01
Charles-Mwaniki <mwaniqicharlie@gmail.com>
2023-08-13 13:24:25 +0300

    Add session management + refresh token route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const Session = require("supertokens-node/recipe/session/framework/express");
+const Session = require("supertokens-node/recipe/session");
+const SessionMiddleware = require("supertokens-node/recipe/session/framework/express");
 
-router.get('/session', Session.verifySession({sessionRequired: false}), async (req, res, next) => {
-    const session = req.session;
+router.get('/login', SessionMiddleware.verifySession({sessionRequired: false}), async (req, res, next) => {
+    let session = req.session;
+    if(!session) {
+        session = await Session.createNewSession(res, '1234');
+    }
     res.send({
         sessionHandle: session.getHandle(),
         userId: session.getUserId(),
@@ -11,5 +15,17 @@ router.get('/session', Session.verifySession({sessionRequired: false}), async (r
         sessionData: await session.getSessionData(),
     });
 });
+
+router.get('/refresh', Session.verifySession({sessionRequired: true}), async (req, res) => {
+    await Session.refreshSession(req, res)
+        .then(async refreshedSession => {
+            console.log(refreshedSession);
+            res.end('Session refreshed');
+        })
+        .catch(err => {
+            console.log('refresh Error:', err);
+            res.end('could not refresh:\n\r' + JSON.stringify(err));
+        });
+})
   
 module.exports = router;
